5a4119873953138c4767343f2f54b46401697365
Brian Day <mrday21@gmail.com>
2019-01-18 02:35:00 +0000

    Added code comments for Stripe.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+    # Whitelist the following form fields so that we can process them. If coming
+    # from a Deivise form.
   before_action :configured_permitted_parameters, if: :devise_controller?
-  
+
   protected
   def configured_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   belongs_to :plan
   
   attr_accessor :stripe_card_token
+  # If Pro user passes validations (email, password, etc..) then call Stripe
+  # and tell Stripe to setup a subscription upon charging the customer's card.
+  # Stripe responds back with customer data.
+  #Store customer.id as customer token and save the the user using Devise gem.
   def save_with_subscription
     if valid?
     customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
