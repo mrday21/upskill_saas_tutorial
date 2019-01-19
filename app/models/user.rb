cc070aff133fd2e06eca4d1a0ad55e2d2b5011d0
Brian Day <mrday21@gmail.com>
2019-01-19 02:58:22 +0000

    Corrected user.rb from card to source

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   #Store customer.id as customer token and save the the user using Devise gem.
   def save_with_subscription
     if valid?
-    customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+    customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token)
     self.stripe_customer_token = customer.id
     save!
     end
