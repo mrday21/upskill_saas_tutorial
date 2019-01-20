0359b71d23e9feea59b4a54d281427c17fa3a5c1
Brian Day <mrday21@gmail.com>
2019-01-20 03:13:30 +0000

    Added profiles create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,4 +5,26 @@ class ProfilesController < ApplicationController
     # Render blank profile details form
     @profile = Profile.new
   end
+  
+  # POST to /users/:user_id/profile
+  def create
+    # Ensure that we have the user who is filling out the form
+    @user = User.find( params[:user_id] )
+    # Create profile linked to this specific user
+    @profile = @user.build_profile( profile_params )
+     if @profile.save
+       flash[:sucess] = "Profile Updated!"
+       redirect_to root_path
+     else
+       #If form fails to save send back to new form field
+       render action: :new
+     end
+  end
+  
+  #Whitelisting
+  private
+  def profile_params
+    params.require(:profile).permit(:first_name, :job_title, :phone_number, 
+    :contact_email, :description)
+  end
 end
